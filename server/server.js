8655d9cace3de61769243818fdd3bfa68647af83
Raghvendra pratap Singh <raghvendrapratapsingh505@gmail.com>
2025-09-10 10:50:16 +0530

    Some Changes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
+dotenv.config();
 import path from 'path';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-dotenv.config();
 
 import express from 'express';
 import cors from 'cors';
